8d19c2f7cc7640b740710dc4a7ca16fb0c9e99e4
Saifmishra <you@example.com>
2022-03-03 17:11:08 +0600

    saif

--- app.js
+++ app.js
@@ -3,32 +3,36 @@ xhr.open('Get', `http://api.openweathermap.org/data/2.5/forecast?q=Dhaka&appid=9
 
 
 let date = new Date();
-let year = date.getFullYear();
 
+//Todays Weather Date
+let year = date.getFullYear();
 let inisialMonth = date.getMonth() + 1;
 let month = inisialMonth.length != 1 ? '0' + inisialMonth : inisialMonth;
-
 let day = date.getDate().length != 1 ? '0' + date.getDate() : date.getDate();
-
 let gettotalDate = year + '-' + month + '-' + day;
-console.log(gettotalDate);
+// console.log(gettotalDate);
 
 
 xhr.onload = function (){
     if(this.status == 200){
         let data = JSON.parse(this.responseText);
-        console.log(data);
+        // console.log(data);
         let todays = [];
        
-
         data.list.forEach(function(data){
             // console.log(data);
             // console.log(data.dt);
             if(data.dt_txt.indexOf(gettotalDate) != -1){
-                console.log(data);
+                // console.log(data);
                 todays.push(data);
             }
-        })
+        });
+        console.log(todays);
+        for(let data in todays){
+            let firstValue = todays[0].main.temp;
+            console.log(todays[data].main.temp);
+        }
+        // let initialTemp = 
     // console.log(this.responseText);
     }
 }
