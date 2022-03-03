1373ec099be49d16818b109a08279deea24714d6
Saifmishra <you@example.com>
2022-03-03 16:44:40 +0600

    main

---
+++ app.js
@@ -0,0 +1,35 @@
+let xhr = new XMLHttpRequest();
+xhr.open('Get', `http://api.openweathermap.org/data/2.5/forecast?q=Dhaka&appid=9f5ea772b64bac3f0beda3dffb54c566`, true);
+
+
+let date = new Date();
+let year = date.getFullYear();
+
+let inisialMonth = date.getMonth() + 1;
+let month = inisialMonth.length != 1 ? '0' + inisialMonth : inisialMonth;
+
+let day = date.getDate().length != 1 ? '0' + date.getDate() : date.getDate();
+
+let gettotalDate = year + '-' + month + '-' + day;
+console.log(gettotalDate);
+
+
+xhr.onload = function (){
+    if(this.status == 200){
+        let data = JSON.parse(this.responseText);
+        console.log(data);
+        let todays = [];
+       
+
+        data.list.forEach(function(data){
+            // console.log(data);
+            // console.log(data.dt);
+            if(data.dt_txt.indexOf(gettotalDate) != -1){
+                console.log(data);
+                todays.push(data);
+            }
+        })
+    // console.log(this.responseText);
+    }
+}
+xhr.send();
